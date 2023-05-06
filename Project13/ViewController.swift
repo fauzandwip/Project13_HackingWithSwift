7707e35c6b601d72757dd718f0fe31e902cc9d72
fauzandp <fauzan27dp@gmail.com>
2023-05-06 09:42:55 +0700

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Fauzan Dwi Prasetyo on 06/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
