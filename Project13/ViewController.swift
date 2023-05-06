e2b4ee9ae73152c26ce633aeddadaffd558a3167
fauzandp <fauzan27dp@gmail.com>
2023-05-06 11:54:58 +0700

    Project13 | Instafilter App add functionality of image picker from library add filter with CoreImage add alertController to choose filter add funcionality change image to filtered while slider changed

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -6,27 +6,102 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
     
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     @IBAction func changeFilter(_ sender: UIButton) {
+        let filters = ["CIBumpDistortion", "CIGaussianBlur", "CIPixellate", "CISepiaTone", "CITwirlDistortion", "CIUnsharpMask", "CIVignette"]
+        
+        let ac = UIAlertController(title: "Choose Filter", message: nil, preferredStyle: .actionSheet)
+        
+        for filter in filters {
+            ac.addAction(UIAlertAction(title: filter, style: .default, handler: setFilter))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else {
+            fatalError("currentImage is nil")
+        }
+        
+        guard let actionTitle = action.title else {
+            fatalError("Action Title not Found")
+        }
+        
+        currentFilter = CIFilter(name: actionTitle)
+        
+        setCIFilterImage(image: currentImage)
+        applyProcessing()
     }
     
     @IBAction func save(_ sender: UIButton) {
     }
     
     @IBAction func intensityChanged(_ sender: UISlider) {
-        print(sender.value)
+        applyProcessing()
     }
     
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+        
+        guard let image = currentFilter.outputImage else { return }
+        
+        if let cgImg = context.createCGImage(image, from: image.extent) {
+            let processedImage = UIImage(cgImage: cgImg)
+            imageView.image = processedImage
+        }
+    }
+    
+    func setCIFilterImage(image: UIImage) {
+        let beginImage = CIImage(image: image)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+//        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
+        setCIFilterImage(image: image)
+        applyProcessing()
+    }
+}
